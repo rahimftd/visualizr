7eb6b7ce0e90e75a94a460af83b2a8b8c5001cf9
Rahim <rahim.dharssi@gmail.com>
2016-04-04 15:00:10 -0700

    Include additional files that should have been part of previous commit

--- server/server.js
+++ server/server.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
 
 var port = 3000;
 
 app.use(express.static(__dirname + '/../app'));
+app.use(bodyParser());
+
+app.use(function(request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/chartscripts', function(request, response) {
+  var chartScriptPath = 'chartScripts/' + request.headers.chartfile;
+  fs.readFile(chartScriptPath, function(error, data) {
+    if(error) {
+      console.log('There was an error reading the file', error);
+      response.send(404);
+    } else {
+      response.send(data);
+    }
+  });
+});
 
 app.listen(port);
 console.log('Server listening on port', port)
